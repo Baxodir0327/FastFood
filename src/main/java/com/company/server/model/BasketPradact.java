bb9152c7baf3cd4f8d415556301e25a3f88fd0cf
doniyorda <davlataliyevdoniyorjon@gmail.com>
2023-07-14 11:57:42 +0500

    s

--- src/main/java/com/company/server/model/BasketPradact.java
+++ src/main/java/com/company/server/model/BasketPradact.java
@@ -1,4 +1,17 @@
 package com.company.server.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class BasketPradact {
+    private Product product;
+
+    private Integer count;
+
+    private boolean clientAdded;
 }
