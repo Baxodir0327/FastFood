93960c51c0c828c23d17e3167e25c67efe499864
Bilol <bilolbekaliqoziyev098@gmail.com>
2023-07-14 12:04:12 +0500

    bilol

--- src/main/java/com/company/server/model/Category.java
+++ src/main/java/com/company/server/model/Category.java
@@ -1,4 +1,9 @@
 package com.company.server.model;
 
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.UUID;
 public class Category {
+
 }
--- src/main/java/com/company/server/model/Order.java
+++ src/main/java/com/company/server/model/Order.java
@@ -1,4 +1,11 @@
 package com.company.server.model;
 
+import lombok.Data;
+
+import java.util.UUID;
+@Data
 public class Order {
+    private UUID id;
+    private MyUser myUser;
+    private String address;
 }
