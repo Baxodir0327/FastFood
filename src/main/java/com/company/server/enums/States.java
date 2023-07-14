ec377ae49f7f7ec09b719eabc3152dcc6d5785a3
Akmal <Akmaldavlatov85@mail.ru>
2023-07-14 14:19:10 +0500

    aaaa

--- src/main/java/com/company/server/enums/States.java
+++ src/main/java/com/company/server/enums/States.java
@@ -1,5 +1,11 @@
 package com.company.server.enums;
 //Akmal aka
 public enum States {
+    MAIN_PAGE,
+    ADDRESS,
+    OLD_ADDRESSES,
+    ROOT_CATEGORY,
+    CHILD_CATEGORY,
+    LOCATION_ACCEPTED,
 
 }
