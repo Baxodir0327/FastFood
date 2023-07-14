b737d9ce9a40d73e2ee084f0a53c5d4a69c5ffd1
doniyorda <davlataliyevdoniyorjon@gmail.com>
2023-07-14 12:01:48 +0500

    s

--- src/main/java/com/company/server/model/BasketProduct.java
+++ src/main/java/com/company/server/model/BasketProduct.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 
 public class BasketProduct{
-
+    private final UUID uuid = UUID.randomUUID();
     private Product product;
-
     private Integer count;
 
     private boolean clientAdded;
