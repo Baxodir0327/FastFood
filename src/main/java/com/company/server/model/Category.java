352afd3f33ef24b1e1f60dde7f49549623e9fa5d
doniyorda <davlataliyevdoniyorjon@gmail.com>
2023-07-14 15:20:00 +0500

    aass

--- src/main/java/com/company/server/model/Category.java
+++ src/main/java/com/company/server/model/Category.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 @Data
 public class Category {
-    private final UUID id;
+    private final UUID id=UUID.randomUUID();
     private String name;
     private Category parent;
     private boolean inline;
---
+++ src/main/java/com/company/server/service/UserService.java
@@ -0,0 +1,5 @@
+package com.company.server.service;
+
+public class UserService {
+
+}
