30fe5c670aca0b40b9b1955640c9c1ba11489121
EnU098 <ergashevnodir1998@gmail.com>
2023-07-14 11:50:16 +0500

    created Basket

--- src/main/java/com/company/server/model/BasketPradact.java
+++ src/main/java/com/company/server/model/BasketPradact.java
@@ -1,24 +1,4 @@
 package com.company.server.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
-
-@Data
-@NoArgsConstructor
 public class BasketPradact {
-    private String id;
-    private Date date;
-    private List<BasketPradact> basketPradacts;
-    private MyUser myUser;
-
-    public BasketPradact(Date date, List<BasketPradact> basketPradacts, MyUser myUser) {
-        this.id = UUID.randomUUID().toString();
-        this.date = date;
-        this.basketPradacts = basketPradacts;
-        this.myUser = myUser;
-    }
 }
