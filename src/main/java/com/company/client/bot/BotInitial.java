eb28ccfd87ca335300f73c1be72116b96cff18c5
Baxodir <bpardaboyev2000@gmail.com>
2023-07-15 12:04:30 +0500

    created bot

---
+++ src/main/java/com/company/client/bot/BotInitial.java
@@ -0,0 +1,14 @@
+package com.company.client.bot;
+
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
+public class BotInitial {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyBot(BotConstants.TOKEN));
+    }
+}
