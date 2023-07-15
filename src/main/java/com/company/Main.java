4996bd63b3b8977116968cc1b24be7b883d280e6
Akhror <axror19977@gmail.com>
2023-07-15 09:07:50 +0500

    deleted all code

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,13 +1,6 @@
 package com.company;
 
-import com.company.server.bot.MyBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new MyBot());
-
+    public static void main(String[] args) {
     }
 }
--- src/main/java/com/company/server/service/BasketService.java
+++
@@ -1,7 +0,0 @@
-package com.company.server.service;
-//Alisher yozyapti
-public class BasketService {
-
-  /// basket shartmasn
-
-}
