9d93fc12f16478fff7c32f2503039e048dacc153
Baxodir <bpardaboyev2000@gmail.com>
2023-07-15 10:26:48 +0500

    models is corrected 100%

--- src/main/java/com/company/server/model/BasketProduct.java
+++ src/main/java/com/company/server/model/BasketProduct.java
@@ -1,11 +1,8 @@
 package com.company.server.model;
 
-import com.company.server.model.BaseModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Getter
 @Setter
--- src/main/java/com/company/server/model/Category.java
+++ src/main/java/com/company/server/model/Category.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.UUID;
-
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
