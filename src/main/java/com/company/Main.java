db06c47eeb5b0d7832e39657d85b7e6fe5a54899
doniyorda <davlataliyevdoniyorjon@gmail.com>
2023-07-15 14:07:14 +0500

    main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
-public class Main {
-    public static void main(String[] args) {
+import com.company.client.bot.MyBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-        System.out.println("Hello world");
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyBot("6384156412:AAFfuQyvX422k63RrMBg1-FrI9R2ZJVTrDk"));
     }
 }
