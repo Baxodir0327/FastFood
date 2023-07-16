455a1f2e732e77569e14d088c5e7039e45ab0f39
EnU098 <ergashevnodir1998@gmail.com>
2023-07-16 16:38:56 +0500

    created chatPage

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.client.bot.BotConstants;
 import com.company.client.bot.MyBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -8,6 +9,6 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(new MyBot("6384156412:AAFfuQyvX422k63RrMBg1-FrI9R2ZJVTrDk"));
+        telegramBotsApi.registerBot(new MyBot(BotConstants.TOKEN));
     }
 }
