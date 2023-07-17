d21185cd81ba4c43a3bb25afe3f63d3e92970fa9
EnU098 <ergashevnodir1998@gmail.com>
2023-07-17 17:32:23 +0500

    commit11

--- src/main/java/com/company/admin/AdminBot.java
+++ src/main/java/com/company/admin/AdminBot.java
@@ -1,11 +1,9 @@
 package com.company.admin;
 
-import com.company.client.bot.MyBot;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class AdminBot extends TelegramLongPollingBot {
 
